194ba683a438037ede66cef525a6580b86b8cbe4
uthree <uthree271828@gmail.com>
2023-07-12 02:05:06 +0900

    wip

--- tests/tests.rs
+++ tests/tests.rs
@@ -11,9 +11,27 @@ pub fn run(code: &str) -> String {
 #[test]
 fn nil() {
     assert_eq!(run(""), "nil");
+    assert_eq!(run("nil"), "nil");
+    assert_eq!(run("()"), "nil");
 }
 
 #[test]
 fn arithmetic() {
     assert_eq!(run("1+1"), "2");
+    assert_eq!(run("1+1+1"), "3");
+}
+
+#[test]
+fn functions() {
+    assert_eq!(run("a = 3; f = (x) -> {x + a}; f(1)"), "4");
+    assert_eq!(run("f = (x, a=1) -> {x + a}; f(1)"), "2");
+    assert_eq!(run("f = (x, a=1) -> {x + a}; f(1, a=2)"), "3");
+}
+
+#[test]
+fn list() {
+    assert_eq!(run("[]"), "[]");
+    assert_eq!(run("[1]"), "[1]");
+    assert_eq!(run("[1, 2, 3]"), "[1, 2, 3]");
+    assert_eq!(run("[1, 2, [3, 4]]"), "[1, 2, [3, 4]]");
 }
